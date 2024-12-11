c288cb1304588d61741bf29614c14c0f4ecb77d6
mitra <dimamarkov320@gmail.com>
2024-12-11 16:39:40 +0300

    Add files via upload

---
+++ name.py
@@ -0,0 +1,26 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    length = len(string)
+    upper = string.upper()
+    lower = string.lower()
+    return(length, upper, lower)
+
+def is_contains(string, list_to_search):
+    count_calls()
+    if string in list_to_search :
+        return True
+    else:
+        return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+
